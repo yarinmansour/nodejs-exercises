f411c54365a1dbe824604645c73b570959055e75
Yarin Mansour <yarinmansour@Yarins-Air-1707.home>
2019-06-17 22:29:41 +0300

    remove dist

--- Session2-exercises/dist/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const products_1 = require("./products");
-exports.products = products_1.default;
-//# sourceMappingURL=index.js.map
--- Session2-exercises/dist/store/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const products = require("./products.json");
-const store = {
-    products,
-};
-exports.store = store;
-//# sourceMappingURL=index.js.map
